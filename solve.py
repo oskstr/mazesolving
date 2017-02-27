39d5f282a913dd377b7915f91d43caec3e97f6e1
Oskar Strömberg <oskstr@kth.se>
2017-02-27 10:48:09 +0100

    Changed variable names.

--- solve.py
+++ solve.py
@@ -7,7 +7,7 @@ should be easy to remove at some point. """
 
 # pylint: disable=C0325, E1101
 
-import numpy as np
+import numpy as np # Unused import
 from PIL import Image
 import time
 from mazes import Maze
@@ -17,49 +17,44 @@ from factory import SolverFactory
 import argparse
 
 def solve(factory, method, input_file, output_file):
+    """ Function docstring placeholder """
     # Load Image
-    print ("Loading Image")
-    im = Image.open(input_file)
+    print("Loading Image")
+    image = Image.open(input_file)
 
-    # Create the maze (and time it) - for many mazes this is more time consuming than solving the maze
-    print ("Creating Maze")
-    t0 = time.time()
-    maze = Maze(im)
-    t1 = time.time()
-    print ("Node Count:", maze.count)
-    total = t1-t0
-    print ("Time elapsed:", total, "\n")
+    # Create the maze (and time it)
+    # - for many mazes this is more time consuming than solving the maze
+    print("Creating Maze")
+    time_0 = time.time()
+    maze = Maze(image)
+    time_1 = time.time()
+    print("Node Count:", maze.count)
+    total = time_1-time_0
+    print("Time elapsed:", total, "\n")
 
     # Create and run solver
     [title, solver] = factory.createsolver(method)
-    print ("Starting Solve:", title)
+    print("Starting Solve:", title)
 
-    t0 = time.time()
+    time_0 = time.time()
     [result, stats] = solver(maze)
-    t1 = time.time()
+    time_1 = time.time()
 
-    total = t1-t0
+    total = time_1-time_0
 
     # Print solve stats
-    print ("Nodes explored: ", stats[0])
-    if (stats[2]):
-        print ("Path found, length", stats[1])
+    print("Nodes explored: ", stats[0])
+    if stats[2]:
+        print("Path found, length", stats[1])
     else:
-        print ("No Path Found")
-    print ("Time elapsed: ", total, "\n")
+        print("No Path Found")
+    print("Time elapsed: ", total, "\n")
 
-    """
-    Create and save the output image.
-    This is simple drawing code that travels between each node in turn, drawing either
-    a horizontal or vertical line as required. Line colour is roughly interpolated between
-    blue and red depending on how far down the path this section is.
-    """
+    print("Saving Image")
+    image = image.convert('RGB')
+    impixels = image.load()
 
-    print ("Saving Image")
-    im = im.convert('RGB')
-    impixels = im.load()
-
-    resultpath = [n.Position for n in result]
+    resultpath = [n.position for n in result]
 
     length = len(resultpath)
 
@@ -69,25 +64,26 @@ def solve(factory, method, input_file, output_file):
 
         # Blue - red
         r = int((i / length) * 255)
-        px = (r, 0, 255 - r)
+        pixel = (r, 0, 255 - r)
 
         if a[0] == b[0]:
             # Ys equal - horizontal line
-            for x in range(min(a[1],b[1]), max(a[1],b[1])):
-                impixels[x,a[0]] = px
+            for x in range(min(a[1], b[1]), max(a[1], b[1])):
+                impixels[x, a[0]] = pixel
         elif a[1] == b[1]:
             # Xs equal - vertical line
-            for y in range(min(a[0],b[0]), max(a[0],b[0]) + 1):
-                impixels[a[1],y] = px
+            for y in range(min(a[0], b[0]), max(a[0], b[0]) + 1):
+                impixels[a[1], y] = pixel
 
-    im.save(output_file)
+    image.save(output_file)
 
 
 def main():
+    """ Function docstring placeholder """
     sf = SolverFactory()
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--method", nargs='?', const=sf.Default, default=sf.Default,
-                        choices=sf.Choices)
+    parser.add_argument("-m", "--method", nargs='?', const=sf.default,
+                        default=sf.default, choices=sf.choices)
     parser.add_argument("input_file")
     parser.add_argument("output_file")
     args = parser.parse_args()
